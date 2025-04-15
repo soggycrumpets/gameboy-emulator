ce09850e6d52f86501b93f9f741c3ffbe90304dd
soggycrumpets <eggsaladfield@gmail.com>
2025-04-14 20:13:50 -0400

    Remove target folder

--- target/debug/deps/gameboy_emulator-264b7528a9f299b6.d
+++
@@ -1,8 +0,0 @@
-/home/soggycrumpets/rust_programs/gameboy-emulator/target/debug/deps/gameboy_emulator-264b7528a9f299b6: src/main.rs src/cpu.rs src/registers.rs src/memory.rs
-
-/home/soggycrumpets/rust_programs/gameboy-emulator/target/debug/deps/gameboy_emulator-264b7528a9f299b6.d: src/main.rs src/cpu.rs src/registers.rs src/memory.rs
-
-src/main.rs:
-src/cpu.rs:
-src/registers.rs:
-src/memory.rs:
--- target/debug/deps/gameboy_emulator-91be40a647fb0daf.d
+++
@@ -1,9 +0,0 @@
-/home/soggycrumpets/rust_programs/gameboy-emulator/target/debug/deps/libgameboy_emulator-91be40a647fb0daf.rmeta: src/lib.rs Cargo.toml
-
-/home/soggycrumpets/rust_programs/gameboy-emulator/target/debug/deps/gameboy_emulator-91be40a647fb0daf.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
